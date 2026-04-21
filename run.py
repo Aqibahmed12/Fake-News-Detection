7be0f68e9f38b681f453100155870cca55a8db30
Aqib Ahmed <f23ari35@aror.edu.pk>
2026-04-21 23:46:29 +0500

    Fix formatting of app.run call in run.py

--- run.py
+++ run.py
@@ -18,4 +18,5 @@ if __name__ == "__main__":
         train_and_save(app.config["MODELS_DIR"], app.config["DATA_DIR"])
         print("[Startup] Model training complete.")
 
-    app.run(debug=True, host="0.0.0.0", port=5000)
+
+app.run(debug=True, host="0.0.0.0", port=5000)
